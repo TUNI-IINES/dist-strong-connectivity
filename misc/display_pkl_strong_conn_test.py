67123359d538081b18c291d2b95508a6d4c75f50
Made Widhi Surya Atman <8626150+mwsatman@users.noreply.github.com>
2022-07-05 08:06:38 +0300

    1000 nodes result

--- misc/display_pkl_strong_conn_test.py
+++ misc/display_pkl_strong_conn_test.py
@@ -3,31 +3,26 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
-#fname = 'temp/test_20220627_022420_PM' # 200 nodes Iterative Link
-# fname = 'temp/test_20220627_023609_PM' # 200 nodes Minimum Link
-# fname = 'temp/test_20220702_021836_PM' # 10 nodes Iterative Link
-# fname = 'temp/test_20220702_021938_PM' # 10 nodes Minimum Link
-# fname = 'temp/test_20220702_042210_PM' # 50 nodes Iterative Link
-# fname = 'temp/test_20220702_042302_PM' # 50 nodes Minimum Link
-# fname = 'temp/test_20220630_040156_PM' # 1000 nodes Iterative Link
-# fname = 'temp/test_20220702_042302_PM' # 1000 nodes Minimum Link
-# test_20220630_040156_PM
-
 # 10 nodes
-# fname = 'temp/graph2_dist'
-# fname = 'temp/graph2_min'
+# fname = 'figure_journal/graph2_dist'
+# fname = 'figure_journal/graph2_min'
 # 20 nodes
-# fname = 'temp/graph3_dist'
-fname = 'temp/graph3_min'
+# fname = 'figure_journal/graph3_dist'
+# fname = 'figure_journal/graph3_min'
 # 50 nodes
-# fname = 'temp/graph5_dist'
-# fname = 'temp/graph5_min'
+# fname = 'figure_journal/graph5_dist'
+# fname = 'figure_journal/graph5_min'
+# 1000 nodes
+# fname = 'figure_journal/graph1000_dist'
+fname = 'figure_journal/graph1000_min'
 
 # widthSizeMul, heightSizeMul = 0.5, 0.6
-widthSizeMul, heightSizeMul = 0.5, 1.2
+# widthSizeMul, heightSizeMul = 0.5, 1.2
 # widthSizeMul, heightSizeMul = 1, 1
+widthSizeMul, heightSizeMul = 2, 2
 
 windowSize = [widthSizeMul*6.4, heightSizeMul*4.8]
+USE_SMALL_NODE = True
 
 
 SHOW_ORIGINAL_GRAPH = False
@@ -55,10 +50,12 @@ with open(fname+'.pkl', 'rb') as f:
         weights = nx.get_edge_attributes(ori_G,'weight').values()
 
         plt.figure(1,figsize=windowSize) 
-        nx.draw(ori_G, Gpos, edge_color=colors, width=list(weights),\
-               with_labels=True, connectionstyle='arc3, rad = 0.1')
-        # nx.draw(ori_G, Gpos, edge_color=colors, width=list(weights),\
-        #     connectionstyle='arc3, rad = 0.1', node_size=10)
+        if USE_SMALL_NODE:
+            nx.draw(ori_G, Gpos, edge_color=colors, width=list(weights),\
+                connectionstyle='arc3, rad = 0.1', node_size=10)
+        else:
+            nx.draw(ori_G, Gpos, edge_color=colors, width=list(weights),\
+                   with_labels=True, connectionstyle='arc3, rad = 0.1')
         plt.legend(ori_dummyLines, ori_labels)
         plt.savefig(fname+'_ori.pdf')
 
@@ -67,10 +64,12 @@ with open(fname+'.pkl', 'rb') as f:
         weights = nx.get_edge_attributes(G,'weight').values()
 
         plt.figure(2,figsize=windowSize) 
-        nx.draw(G, Gpos, labels=mapping, edge_color=colors, width=list(weights),\
-               with_labels=True, connectionstyle='arc3, rad = 0.1')
-        # nx.draw(G, Gpos, edge_color=colors, width=list(weights),\
-        #     connectionstyle='arc3, rad = 0.1', node_size=10)
+        if USE_SMALL_NODE:
+            nx.draw(G, Gpos, edge_color=colors, width=list(weights),\
+                connectionstyle='arc3, rad = 0.1', node_size=10)
+        else:
+            nx.draw(G, Gpos, labels=mapping, edge_color=colors, width=list(weights),\
+                   with_labels=True, connectionstyle='arc3, rad = 0.1')
         plt.legend(dummyLines, labels)
         plt.savefig(fname+'.pdf')
         plt.show()
